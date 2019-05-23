7f3b3fbd414d9836dbd6d6ca8d1006d3c3fe217b
Travis Exline <t.exline22@gmail.com>
2019-05-23 13:35:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    array.select {|new_anagram| new_anagram.split("").sort == @name.split("").sort}
+  end
+end
