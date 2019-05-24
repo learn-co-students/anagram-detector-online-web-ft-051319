a01084159fd1e56bb2d1078ee05e9dfeec4cc440
Haley DeKeyser <hcd09@live.com>
2019-05-24 03:39:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(potential_anagrams)
+    %w(potential_anagrams)
+    potential_anagrams.find_all do |some_word|
+      some_word.split("").sort == @word.split("").sort
+    end
+  end
+end
