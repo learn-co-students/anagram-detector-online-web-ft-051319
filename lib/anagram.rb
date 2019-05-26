1967e05394743c0bc696c8fc0f7fe810876b5ba9
Angela Mumba <malaika.mumba@gmail.com>
2019-05-26 05:04:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,11 @@
-# Your code goes here!
 class Anagram
   attr_accessor :word
 
      def initialize(word)
        @word = word
-       # @match = match
      end
 
       def match(words)
-        words.select do|w|
-        w.split(" ").sort == @word.split(" ").sort && w.split("").length == @word.length
-
+     words.select {|w|w.split("").sort == @word.split("").sort}
       end
-    end
 end
