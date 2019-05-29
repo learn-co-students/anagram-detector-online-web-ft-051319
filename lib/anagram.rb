c1127d8dd90fdf9c0b088ac6ef14d62012fa2b0a
M Banks <montensia1@outlook.com>
2019-05-29 22:23:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :match_word
+  def initialize(word)
+    @match_word = word
+  end
+  
+  def match(words_arr)
+    words_arr.select do |word| 
+      @match_word.split("").sort == word.split("").sort
+    end
+    #words_arr.find {|word| word.split("").sort == @match_word.split("").sort}
+  
+  end
+  
+end
